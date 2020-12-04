c28e84779743ab77b158ab0267471770191dd183
lyndsp <lyndsayp@gmail.com>
2020-12-04 09:07:12 +0000

    Day 1 solved!
    Co-authored-by: Adam Straughan <astraughan@mapalsoftware.com>

--- find2020Pair.go
+++ find2020Pair.go
@@ -1,6 +1,21 @@
 package integers
 
-// Find2020Pair returns true if the input adds to 2020
-func Find2020Pair(x, y int) bool {
-	return x+y == 2020
+// Find2020Rhs returns true if the lhs and an element in the rhs add to 2020
+func indexOfFirstToSum(sum int, input []int) (int, int) {
+	for index := 0; index < len(input); index++ {
+		current := input[index]
+		theRest := input[index+1:]
+		for i, v := range theRest {
+			if current+v == sum {
+				return index, i + index + 1
+			}
+		}
+	}
+	return -1, -1
 }
+
+// Find2020Pair returns true if two elements in the input add to 2020
+// func Find2020Pair(input []int) bool {
+
+// 	return false
+// }
--- find2020Pair_test.go
+++ find2020Pair_test.go
@@ -1,12 +1,45 @@
 package integers
 
-import "testing"
+import (
+	"testing"
 
-func TestFind2020Pair(t *testing.T) {
-	result := Find2020Pair(2000, 20)
-	expected := true
+	"github.com/stretchr/testify/assert"
+)
 
-	if result != expected {
-		t.Errorf("expected '%v' but got '%v'", expected, result)
+func Test_when_no_match_nothing_is_found(t *testing.T) {
+
+	i, j := indexOfFirstToSum(2020, []int{1, 2, 3})
+
+	assert.Equal(t, -1, i)
+	assert.Equal(t, -1, j)
+}
+
+func Test_when_sum_is_found_indices_returned(t *testing.T) {
+
+	testData := []struct {
+		name          string
+		sum           int
+		slice         []int
+		first, second int
+	}{
+		{"first plus second", 3, []int{1, 2, 3}, 0, 1},
+		{"second plus third", 5, []int{1, 2, 3}, 1, 2},
+		{"non-consecutive indexes", 4, []int{1, 2, 3}, 0, 2},
+		{"no sum found", 99, []int{1, 2, 3}, -1, -1},
+		{"actual test input", 2020, adventTestValues, -1, -1},
 	}
+
+	for _, td := range testData {
+		t.Run(td.name, func(t *testing.T) {
+			i, j := indexOfFirstToSum(td.sum, td.slice)
+
+			assert.Equal(t, td.first, i)
+			assert.Equal(t, td.second, j)
+		})
+	}
+}
+
+func Test_the_answer(t *testing.T) {
+	i, j := indexOfFirstToSum(2020, adventTestValues)
+	assert.Equal(t, 0, adventTestValues[i]*adventTestValues[j])
 }
